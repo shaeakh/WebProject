5f7ffc56058fe53e956ca405f10dccd26fe81d04
Md. Shaeakh Ahmed Chowdhury <shaeakh12@gmail.com>
2024-05-30 02:21:57 +0600

    Register Page UI added

--- client/src/app/Loginpage2/page.tsx
+++ client/src/app/Loginpage2/page.tsx
@@ -1,8 +1,28 @@
 "use client";
 import React from "react";
+import { Button } from "@/components/ui/button"
+import SubmitBtn from "../components/SubmitBtn";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { cn } from "@/utils/cn";
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+
 import {
   IconBrandGithub,
   IconBrandGoogle,
@@ -14,87 +34,76 @@ export default function SignupFormDemo() {
     e.preventDefault();
     console.log("Form submitted");
   };
+  const [depertment, setDepertment] = React.useState("Depertment");
   return (
-    <div className="max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black">
+
+    <div className="flex h-screen w-screen justify-center items-center">
+      <div className="max-w-md w-screen mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black">
       <h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200">
-        Welcome to Aceternity
+        Welcome to Online Player Auction
       </h2>
       <p className="text-neutral-600 text-sm max-w-sm mt-2 dark:text-neutral-300">
-        Login to aceternity if you can because we don&apos;t have a login flow
-        yet
+        Get the best players in the tournament with our analysis tools
       </p>
 
       <form className="my-8" onSubmit={handleSubmit}>
         <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
           <LabelInputContainer>
-            <Label htmlFor="firstname">First name</Label>
-            <Input id="firstname" placeholder="Tyler" type="text" />
-          </LabelInputContainer>
-          <LabelInputContainer>
-            <Label htmlFor="lastname">Last name</Label>
-            <Input id="lastname" placeholder="Durden" type="text" />
+            <Label htmlFor="firstname">Name</Label>
+            <Input id="firstname" placeholder="John Doe" type="text" />
           </LabelInputContainer>
         </div>
         <LabelInputContainer className="mb-4">
-          <Label htmlFor="email">Email Address</Label>
-          <Input id="email" placeholder="projectmayhem@fc.com" type="email" />
+          <Label htmlFor="phone">Phone Number</Label>
+          <Input id="phone" placeholder="Enter phone number" type="tel" />
+        </LabelInputContainer>
+        <LabelInputContainer className="mb-4">
+          <Label htmlFor="email">Educational Email Address</Label>
+          <Input id="email" placeholder="xyz12@student.sust.edu" type="email" />
+        </LabelInputContainer>
+        <LabelInputContainer className="mb-4">
+          <Label htmlFor="studentRegistrationNo">Student Registration Number</Label>
+          <Input id="studentRegistrationNo" placeholder="Enter Registration Number" type="text" />
         </LabelInputContainer>
         <LabelInputContainer className="mb-4">
           <Label htmlFor="password">Password</Label>
           <Input id="password" placeholder="••••••••" type="password" />
         </LabelInputContainer>
-        <LabelInputContainer className="mb-8">
-          <Label htmlFor="twitterpassword">Your twitter password</Label>
-          <Input
-            id="twitterpassword"
-            placeholder="••••••••"
-            type="twitterpassword"
-          />
+        <LabelInputContainer className="mb-4">
+          <Label htmlFor="password">Confirm Password</Label>
+          <Input id="password" placeholder="••••••••" type="password" />
         </LabelInputContainer>
+        <div className="mb-4">
+          <DropdownMenu >
+            <DropdownMenuTrigger asChild >
+              <Button variant={"outline"}>{depertment}</Button>
+            </DropdownMenuTrigger>
 
-        <button
-          className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
-          type="submit"
-        >
-          Sign up &rarr;
-          <BottomGradient />
-        </button>
-
-        <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-8 h-[1px] w-full" />
-
-        <div className="flex flex-col space-y-4">
-          <button
-            className=" relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
-            type="submit"
-          >
-            <IconBrandGithub className="h-4 w-4 text-neutral-800 dark:text-neutral-300" />
-            <span className="text-neutral-700 dark:text-neutral-300 text-sm">
-              GitHub
-            </span>
-            <BottomGradient />
-          </button>
-          <button
-            className=" relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
-            type="submit"
-          >
-            <IconBrandGoogle className="h-4 w-4 text-neutral-800 dark:text-neutral-300" />
-            <span className="text-neutral-700 dark:text-neutral-300 text-sm">
-              Google
-            </span>
-            <BottomGradient />
-          </button>
-          <button
-            className=" relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
-            type="submit"
-          >
-            <IconBrandOnlyfans className="h-4 w-4 text-neutral-800 dark:text-neutral-300" />
-            <span className="text-neutral-700 dark:text-neutral-300 text-sm">
-              OnlyFans
-            </span>
-            <BottomGradient />
-          </button>
+            <DropdownMenuContent className="w-56">
+              <DropdownMenuLabel>Depertments</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuRadioGroup value={depertment} onValueChange={setDepertment}>
+                
+                <DropdownMenuRadioItem value="ARC">ARC</DropdownMenuRadioItem>
+                <DropdownMenuRadioItem value="CSE">CSE</DropdownMenuRadioItem>
+                <DropdownMenuRadioItem value="SWE">SWE</DropdownMenuRadioItem>
+                <DropdownMenuRadioItem value="EEE">EEE</DropdownMenuRadioItem>
+                <DropdownMenuRadioItem value="IPE">IPE</DropdownMenuRadioItem>
+                <DropdownMenuRadioItem value="ME">ME</DropdownMenuRadioItem>
+                <DropdownMenuRadioItem value="CEE">CEE</DropdownMenuRadioItem>
+                <DropdownMenuRadioItem value="PME">PME</DropdownMenuRadioItem>
+                <DropdownMenuRadioItem value="BMB">BMB</DropdownMenuRadioItem>
+                <DropdownMenuRadioItem value="GEB">GEB</DropdownMenuRadioItem>
+                <DropdownMenuRadioItem value="PSS">PSS</DropdownMenuRadioItem>
+                
+              </DropdownMenuRadioGroup>
+            </DropdownMenuContent>
+          </DropdownMenu>
         </div>
+        <SubmitBtn text={"black"} bg={"white"} borderclr={"black"} hover_bg={"black"} hover_text={"white"} hover_border={"white"}
+          value={"Register"} />
       </form>
+    </div>
     </div>
   );
 }
