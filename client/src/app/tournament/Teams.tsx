b50090be42ddfb7e49ddc926af6733e08983dfde
Md. Shaeakh Ahmed Chowdhury <shaeakh12@gmail.com>
2024-06-04 23:12:06 +0600

    .

--- client/src/app/tournament/Teams.tsx
+++ client/src/app/tournament/Teams.tsx
@@ -1,41 +1,10 @@
 import { HoverEffect } from "@/app/tournament/SCcard-hover-effect";
 
-export default function Teams() {
+export default function Teams(props:any) {
+  let obj = props.obj;
   return (
-    <div className="w-8/12 mx-auto ">
-      <HoverEffect items={projects} />
+    <div className="w-full mx-auto ">
+      <HoverEffect items={obj} />
     </div>
   );
 }
-export const projects = [
-  {
-    team: "Argentina",
-    manager: "Neimar",
-    link : "https://images2.alphacoders.com/980/thumb-1920-980120.jpg"    
-  },
-  {
-    team: "Argentina",
-    manager: "Neimar",
-    link : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXodY1a9enLLajV23mvqoSOPxqZwuGZ-laWQ&s"    
-  },
-  {
-    team: "Argentina",
-    manager: "Neimar",
-    link : "https://images2.alphacoders.com/980/thumb-1920-980120.jpg"    
-  },
-  {
-    team: "Argentina",
-    manager: "Neimar",
-    link : "https://images2.alphacoders.com/980/thumb-1920-980120.jpg"    
-  },
-  {
-    team: "Argentina",
-    manager: "Neimar",
-    link : "https://images2.alphacoders.com/980/thumb-1920-980120.jpg"    
-  },
-  {
-    team: "Argentina",
-    manager: "Neimar",
-    link : "https://images2.alphacoders.com/980/thumb-1920-980120.jpg"    
-  }  
-];
--- client/src/app/tournament/page.tsx
+++ client/src/app/tournament/page.tsx
@@ -7,63 +7,83 @@ import React from 'react'
 import { FaRegCopy } from "react-icons/fa6";
 import { MoviingBorderButton } from "@/components/ui/SCmoving-border";
 import Teams from './Teams'
-function page() {
+import Teamheader from './Teamheader'
+
+function Page() {
     let props = {
         joincode: "#asdksadsa"
     }
-    return (
-        <div className='w-screen flex flex-col justify-center items-center p-5'>
-            <div className='w-8/12 h-2/6  flex items-center justify-center overflow-hidden rounded-lg'>
-                <img
-                    src="https://staticg.sportskeeda.com/editor/2022/05/17c93-16519157288727-1920.jpg"
-                    alt=""
-                    className='w-full h-full object-cover'
-                />
+    let user = {
+        role: "manager",
+        card: [{
+            team: "Argentina",
+            manager: "Shaeakh",
+            link: "https://images2.alphacoders.com/980/thumb-1920-980120.jpg"
+        }]
+    }
 
+    return (
+        <div className='flex flex-col justify-center items-center p-5'>
+            <div className='w-8/12 lg:h-56 md:h-40 sm:h-24 overflow-hidden rounded-lg flex justify-center border-2 border-black'>
+                <img className='object-cover h-full w-full' src="https://staticg.sportskeeda.com/editor/2022/05/17c93-16519157288727-1920.jpg" alt="" />
             </div>
-            
-            <div className='flex justify-between  w-8/12 p-2 '>
-                <div className='h-32 flex flex-col justify-center items-around'>
-                    <div className='flex flex-col  justify-around p-2 border border-black rounded-lg' >
-                        <p className='text-2xl font-bold'>Tournament Join Code</p>
-                        <hr className='w-full border border-grey' />
-                        <div className='flex justify-between p-2'>
-                            <div className=' w-9/12 bg-black rounded-lg bg-opacity-50 py-2 px-4'>
-                                <p className='text-xl text-white font-bold '>{props.joincode}</p>
+            {user.role === "admin" && (
+                <div className='border-2 w-8/12 border-black'>
+                    <div className='flex justify-between w-full p-2'>
+                        <div className='h-32 flex flex-col justify-center'>
+                            <div className='flex flex-col justify-around p-2 border border-black rounded-lg'>
+                                <p className='text-2xl font-bold'>Tournament Join Code</p>
+                                <hr className='w-full border border-grey' />
+                                <div className='flex justify-between p-2'>
+                                    <div className='w-9/12 bg-black rounded-lg bg-opacity-50 py-2 px-4'>
+                                        <p className='text-xl text-white font-bold'>{props.joincode}</p>
+                                    </div>
+                                    <button className="px-4 py-2 w-min rounded-md bg-black text-white font-bold text-sm hover:-translate-y-1 transform transition duration-200 hover:shadow-md">
+                                        <FaRegCopy className='h-full' />
+                                    </button>
+                                </div>
                             </div>
-                            <button className="px-4 py-2 w-min rounded-md bg-black text-white font-bold text-sm hover:-translate-y-1 transform transition duration-200 hover:shadow-md">
-                                <FaRegCopy className='h-full' />
+                        </div>
+                        <div className='flex flex-col justify-center'>
+                            <div>
+                                <MoviingBorderButton
+                                    borderRadius="1.75rem"
+                                    className="bg-white hover:bg-black hover:text-white transition transition-colors duration-500 font-bold text-xl text-black border-2 border-neutral-200"
+                                >
+                                    Start Auction
+                                </MoviingBorderButton>
+                            </div>
+                        </div>
+                        <div className='flex flex-col gap-2'>
+                            <button className="px-8 py-2 rounded-md bg-black text-white font-bold text-sm hover:-translate-y-1 transform transition duration-200 hover:shadow-md">
+                                Member Request
+                            </button>
+                            <button className="px-8 py-2 rounded-md bg-black text-white font-bold text-sm hover:-translate-y-1 transform transition duration-200 hover:shadow-md">
+                                Set Players Category
+                            </button>
+                            <button className="px-8 py-2 rounded-md bg-black text-white font-bold text-sm hover:-translate-y-1 transform transition duration-200 hover:shadow-md">
+                                Update Tournament
                             </button>
                         </div>
+                    </div>
+                    <hr className='border border-grey w-full my-2' />
+                    <Teams />
+                </div>
+            )}
+            {(user.role === "manager" || user.role === "player") && (
+                <div className='w-8/12 border-black flex flex-col'>
 
+                    <div className='flex w-full '>
+                        <Teamheader/>                                         
                     </div>
+
                 </div>
-                <div className='flex flex-col justify-center'>
-                    <div >
-                        <MoviingBorderButton
-                            borderRadius="1.75rem"
-                            className="bg-white hover:bg-black hover:text-white transition transition-colors duration-500 font-bold text-xl text-black border-2 border-neutral-200"
-                        >
-                            Start Auction
-                        </MoviingBorderButton>
-                    </div>
-                </div>
-                <div className='flex flex-col items-around gap-2'>
-                    <button className="px-8 py-2 rounded-md bg-black text-white font-bold text-sm hover:-translate-y-1 transform transition duration-200 hover:shadow-md">
-                        Member Request
-                    </button>
-                    <button className="px-8 py-2 rounded-md bg-black text-white font-bold text-sm hover:-translate-y-1 transform transition duration-200 hover:shadow-md">
-                        Set players catagory
-                    </button>
-                    <button className="px-8 py-2 rounded-md bg-black text-white font-bold text-sm hover:-translate-y-1 transform transition duration-200 hover:shadow-md">
-                        Update Tournament
-                    </button>
-                </div>
-            </div>
-            <hr className='border border-grey w-8/12 my-2' />
-            <Teams/>
+            )}
+            {user.role !== "admin" && user.role !== "manager" && user.role !== "player" && (
+                <div>Invalid user!</div>
+            )}
         </div>
-        
     )
 }
-export default page
+
+export default Page
